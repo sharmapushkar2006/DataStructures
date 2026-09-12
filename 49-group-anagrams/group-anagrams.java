75f66b6b467af09164a777407295a149d4fca5e7
Pushkar Sharma <258246915+sharmapushkar2006@users.noreply.github.com>
2026-09-12 21:32:55 +0530

    Time: 6 ms (99.03%) | Memory: 49.9 MB (25.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -13,3 +13,6 @@ class Solution {
         return new ArrayList<>(data.values());
     }
 }
+// what i did in this question::
+//   We created a hashmap and then we will start a for loop which will go to each value in strs and then sort it, after that it check if there is any key in our hashmap which is same as the value at strs[i] 
+//   if true then it simply add the strs[i] to that arraylist else it will create a key with empty arraylist in it and add it to the hash
